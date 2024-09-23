47ebac9b55eb0dcffbf40fa4479e24a09c8ea91d
sumayyadatabeat <sumayya.makkiya@databeat.io>
2024-09-23 20:41:43 +0530

    error

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -8,35 +8,10 @@ import Image from "next/image";
 const EmailSection = () => {
   const [emailSubmitted, setEmailSubmitted] = useState(false);
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
-
-    // Form the request for sending data to the server.
-    const options = {
-      // The method is POST because we are sending data.
-      method: "POST",
-      // Tell the server we're sending JSON.
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // Body of the request is the JSON data we created above.
-      body: JSONdata,
-    };
-
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-
-    if (response.status === 200) {
-      console.log("Message sent.");
-      setEmailSubmitted(true);
-    }
+    // Simulate form submission without API call
+    setEmailSubmitted(true);
   };
 
   return (
@@ -67,7 +42,7 @@ const EmailSection = () => {
       <div>
         {emailSubmitted ? (
           <p className="text-green-500 text-sm mt-2">
-            Email sent successfully!
+            Email submitted successfully!
           </p>
         ) : (
           <form className="flex flex-col" onSubmit={handleSubmit}>
